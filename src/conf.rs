f2e73ebb89a10cfafcccba37948ecccacf86ecf9
capnhawkbill <capnhawkbill@airmail.cc>
2021-05-14 22:33:25 +0200

    serialize and deserialize for keys

--- src/conf.rs
+++ src/conf.rs
@@ -1,6 +1,12 @@
+use serde::{
+    de::{Deserializer, Visitor},
+    ser::Serializer,
+    Deserialize, Serialize,
+};
 use termion::event::Key;
 use tui::style::{Color, Modifier, Style};
 
+#[derive(Serialize, Deserialize)]
 pub struct Config {
     pub path: String,
     pub theme: Theme,
@@ -19,6 +25,14 @@ impl Default for Config {
     }
 }
 
+impl Config {
+    fn load(file: Path) -> Result<Config, std::io::Error> {
+        let contents = std::fs::read_to_string(file)?;
+        Ok(toml::from_str(&contents))
+    }
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct Theme {
     pub title: Style,
     pub comment: Style,
@@ -39,16 +53,124 @@ impl Default for Theme {
     }
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Keybinds {
-    pub quit: Key,
-    pub search: Key,
+    pub quit: TerKey,
+    pub search: TerKey,
 }
 
 impl Default for Keybinds {
     fn default() -> Self {
         Keybinds {
-            quit: Key::Ctrl('c'),
-            search: Key::Char('/'),
+            quit: TerKey(Key::Ctrl('c')),
+            search: TerKey(Key::Char('/')),
         }
+    }
+}
+
+/// Termion key wrapper that has serialize and deserialize
+struct TerKey(Key);
+
+impl std::ops::Deref for TerKey {
+    type Target = Key;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Serialize for TerKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let string = match self.0 {
+            Key::Backspace => "Backspace".to_string(),
+            Key::Left => "Left".to_string(),
+            Key::Right => "Right".to_string(),
+            Key::Up => "Up".to_string(),
+            Key::Down => "Down".to_string(),
+            Key::Home => "Home".to_string(),
+            Key::End => "End".to_string(),
+            Key::PageUp => "PageUp".to_string(),
+            Key::PageDown => "PageDown".to_string(),
+            Key::BackTab => "BackTab".to_string(),
+            Key::Delete => "Delete".to_string(),
+            Key::Insert => "Insert".to_string(),
+            Key::F(n) => format!("F{}", n),
+            Key::Char(c) => c.to_string(),
+            Key::Alt(c) => format!("Alt+{}", c),
+            Key::Ctrl(c) => format!("Ctrl+{}", c),
+            Key::Null => "Null".to_string(),
+            Key::Esc => "Esc".to_string(),
+            Key::__IsNotComplete => unreachable![],
+        };
+        serializer.serialize_str(&string)
+    }
+}
+
+impl<'de> Deserialize<'de> for TerKey {
+    fn deserialize<D>(deserializer: D) -> Result<TerKey, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(KeyVisitor)
+    }
+}
+
+struct KeyVisitor;
+
+impl<'de> Visitor<'de> for KeyVisitor {
+    type Value = TerKey;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(formatter, "a string containing a valid keycode")
+    }
+
+    fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        let key = match s {
+            "Backspace" => Key::Backspace,
+            "Left" => Key::Left,
+            "Right" => Key::Right,
+            "Up" => Key::Up,
+            "Down" => Key::Down,
+            "Home" => Key::Home,
+            "End" => Key::End,
+            "PageUp" => Key::PageUp,
+            "PageDown" => Key::PageDown,
+            "BackTab" => Key::BackTab,
+            "Delete" => Key::Delete,
+            "Insert" => Key::Insert,
+            s if s.starts_with("F") && s.len() == 2 => {
+                // FIXME unwraps
+                let n = s
+                    .chars()
+                    .skip(1)
+                    .next()
+                    .unwrap()
+                    .to_string()
+                    .parse::<u8>()
+                    .unwrap();
+                Key::F(n)
+            }
+            "Null" => Key::Null,
+            "Esc" => Key::Esc,
+            s if s.len() == 1 => Key::Char(s.chars().next().unwrap()),
+            s if s.starts_with("Alt+") && s.len() == 5 => {
+                // FIXME unwraps
+                let c = s.chars().skip(1).next().unwrap();
+                Key::Alt(c)
+            }
+            s if s.starts_with("Ctrl+") && s.len() == 6 => {
+                // FIXME unwraps
+                let c = s.chars().skip(1).next().unwrap();
+                Key::Ctrl(c)
+            }
+        };
+
+        Ok(TerKey(key))
     }
 }
